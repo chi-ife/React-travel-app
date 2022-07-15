c85070ff62d2403c568142460424103a43fe7ed6
sltheweirdo <dcileka@gmail.com>
2022-07-15 03:18:18 +0100

    Initial commit

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Button.css'
+import { Link } from 'react-router-dom';
+
+export const Button = ({children, type, onclick, buttonStyle,
+buttonSize}) => {
+    const checkButtonStyle = STYLES.icludes(buttonStyle)
+}
